20e01994afea04c08cd050a3dc0b7dc927e1cc1e
AlbertWi <160854652+AlbertWi@users.noreply.github.com>
2025-06-10 18:13:34 +0700

    Deploy

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('welcome');
 });
 
 Route::resource('/fakultas', FakultasController::class);
